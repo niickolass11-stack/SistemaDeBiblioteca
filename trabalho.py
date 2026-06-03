4e240f9175a4e38485b3889f1ab59138808ee1d1
Nickolas <niickolass11@gmail.com>
2026-06-03 20:37:55 -0300

    Complementaçao do trabalho

--- trabalho.py
+++ trabalho.py
@@ -8,28 +8,45 @@ listaLivros: list = []
 def CadastrarLivros():
 
     cadastrarLivros = str(input("Informe o nome do livro a ser cadastrado: ")).upper()
-    listaLivros.append(cadastrarLivros)
+    
 
-    # Se o meu livro cadastrado ja exixtir dentro da minha lista eu aviso ao usuario que o livro ja existe
+    if cadastrarLivros in listaLivros:
 
-    print("\nCadastro Realizado Com Sucesso.\n")
-    print("Redirecionando ao menu Inicial...\n")
+        print("Livro já cadastrado")
+        print("Redirecionando ao menu Inicial...\n")
+    
+    else:
+        
+        listaLivros.append(cadastrarLivros)
+        
+        print("\nCadastro Realizado Com Sucesso.\n")
+        print("Redirecionando ao menu Inicial...\n")
+
 
 def VerLivrosCadastrados():
 
     listaLivros.sort()
-    print(listaLivros)
+    for livros in listaLivros:
+        print(livros)
 
     # se a minha lista de livros estiver vazia eu aviso ao usuario que a lista esta vazia e encaminho o usuario ao menu inicial ou a pagina de cadastro de livos
 
 def RemoverLivros():
 
     remover = str(input("Informe o livro que deseja remover: ")).upper()
-    listaLivros.remove(remover)
     
-    # se o usuario digitar o nome errado eu dou uma mensagem ao usuario dizendo que este livro nao existe e dou a lista dos livros existentes para ele 
-    print("\nLivro Removido Com Sucesso.\n")
-    print("Redirecionando a Página Inicial...\n")
+
+    if remover not in listaLivros:
+
+        print("Nome incorreto impossivel remover o livro")
+        print("Redirecionando a Página Inicial...\n")
+    
+    else:
+
+        listaLivros.remove(remover)
+        
+        print("\nLivro Removido Com Sucesso.\n")
+        print("Redirecionando a Página Inicial...\n")
 
 
 def Menu():
